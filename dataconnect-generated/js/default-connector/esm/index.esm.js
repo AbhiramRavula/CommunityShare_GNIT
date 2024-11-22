4a2da25ebbaa703b461f3800db5119a74e654ea1
AbhiramRavula <138038324+AbhiramRavula@users.noreply.github.com>
2024-11-22 11:18:13 +0530

    hosted on web

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'CommunityShare_GNIT',
+  location: 'us-central1'
+};
+
